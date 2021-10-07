db7044e92e86003804fbb4e80ab46f4aec3cb47f
Maxime Leriche <maxleriche.60@gmail.com>
2021-10-07 23:02:31 +0200

    [add] csharp POWWWWWWWWWAAAAAAAAAAAAAA

---
+++ ApplicationDiscord/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApplicationDiscord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
