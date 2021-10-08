bc81a9ed182e09dd6d9c773b73a80f8351b39ed6
Maxime Leriche <maxleriche.60@gmail.com>
2021-10-08 09:08:45 +0200

    [add] Pré implémentation de discord

--- ApplicationDiscord/Program.cs
+++ ApplicationDiscord/Program.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
 
 namespace ApplicationDiscord
 {
     class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+        public static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private Task Log(LogMessage msg)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
+        private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+        {
+            Console.WriteLine("Trigger messageUpdated");
+            // If the message was not in the cache, downloading it will result in getting a copy of `after`.
+            var message = await before.GetOrDownloadAsync();
+            Console.WriteLine($"{message} -> {after}");
+        }
+
+        private async Task NewMessage(SocketMessage newMsg)
+        {
+            Console.WriteLine($"{newMsg.Timestamp.UtcDateTime} : {newMsg}");
+        }
+
+        public async Task MainAsync()
+        {
+            var _config = new DiscordSocketConfig { MessageCacheSize = 100 };
+            _client = new DiscordSocketClient(_config);
+            _client.Log += Log;
+            await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("BotToken"));
+            await _client.StartAsync();
+            _client.MessageUpdated += MessageUpdated;
+            _client.MessageReceived += NewMessage;
+            _client.Ready += () =>
+            {
+                Console.WriteLine("Bot is connected!");
+                return Task.CompletedTask;
+            };
+            await Task.Delay(-1);
+        }
+        
     }
 }
