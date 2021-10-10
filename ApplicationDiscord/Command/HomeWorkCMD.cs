af00d38518cf5a898c844f509e421049f9bed94b
Maxime Leriche <maxleriche.60@gmail.com>
2021-10-11 00:07:17 +0200

    [add] Create get and create homework function

--- ApplicationDiscord/Command/HomeWorkCMD.cs
+++ ApplicationDiscord/Command/HomeWorkCMD.cs
@@ -1,5 +1,8 @@
 ﻿using Discord.Commands;
 using System.Threading.Tasks;
+using Domain.Handler;
+using Domain.Model;
+using System;
 
 namespace ApplicationDiscord.Command
 {
@@ -7,10 +10,40 @@ namespace ApplicationDiscord.Command
     [Summary("HomeWork allow the user to handle the basic HomeWork")]
     public class HomeWorkCMD : ModuleBase<SocketCommandContext>
     {
+        public HandleHomeWork handlehomeWork { get; set; }
         [Command]
         public async Task DefaultHomeWorkCMD()
         {
             await Context.Channel.SendMessageAsync("Hello World");
+        }
+
+        [Command("get")]
+        public async Task getAllHomeWork()
+        {
+            if (handlehomeWork.GetHomeWork().Count == 0)
+            {
+                await Context.Channel.SendMessageAsync("No Homework yet");
+            }
+            else
+                foreach(HomeWork homework in handlehomeWork.GetHomeWork())
+                {
+                    await Context.Channel.SendMessageAsync(homework.ToString());
+                }
+        }
+
+        [Command("create")]
+        public async Task createHomeWork(HomeWorkParameter homeWorkParameter)
+        {   
+            HomeWork newHomeWork = new HomeWork();
+            newHomeWork.Label = homeWorkParameter.Label;
+            newHomeWork.Matter = homeWorkParameter.Matter;
+            newHomeWork.Content = homeWorkParameter.Content;
+            DateTime whenToReturn;
+            DateTime.TryParse(homeWorkParameter.Due, out whenToReturn);
+            newHomeWork.Return = whenToReturn;
+            newHomeWork.By = Context.Message.Author.Id.ToString();
+            handlehomeWork.CreateHomeWork(newHomeWork);
+            await ReplyAsync($"{homeWorkParameter.Label} : has been created");
         }
     }
 }
---
+++ ApplicationDiscord/Command/HomeWorkParameter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Model;
+using Discord.Commands;
+
+namespace ApplicationDiscord.Command
+{
+    [NamedArgumentType]
+    public class HomeWorkParameter
+    {
+        public string Label { get; set; } 
+        public  string Matter { get; set; } = null;
+        public  string Due { get; set; } = null;
+        public  string Content { get; set; } = null;
+    }
+}
