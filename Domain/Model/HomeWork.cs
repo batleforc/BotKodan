0bb92886e6f6dd6169a5f6cab39c4531c16bd356
Maxime Leriche <maxleriche.60@gmail.com>
2021-10-11 00:05:55 +0200

    [add] constructor/accessor/ToString to Homework Model

--- Domain/Model/HomeWork.cs
+++ Domain/Model/HomeWork.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,41 @@ namespace Domain.Model
 {
     public class HomeWork : IModel
     {
-        string Label;
-        DateTime Created;
-        string By;
-        DateTime Return;
-        String Content;
+        public string Label {  get;  set; }
+        public string Matter {  get;  set; }
+        public DateTime Created {  get;  set; }
+        public string By {  get;  set; }
+        public DateTime Return {  get; set; }
+        public string Content {  get; set; }
+
+        public HomeWork()
+        {
+
+        }
+
+        public HomeWork(HomeWork homeWork)
+        {
+            Console.WriteLine("John: "+homeWork.ToString());
+            Label = homeWork.Label;
+            Matter = homeWork.Matter;
+            Created = homeWork.Created;
+            By = homeWork.By;
+            Return = homeWork.Return;
+            Content = homeWork.Content;
+        }
+        public HomeWork(string label, string matter, DateTime created,string by,DateTime whentoreturn,string content)
+        {
+            Label = label;
+            Matter = matter;
+            Created = created;
+            By = by;
+            Return = whentoreturn;
+            Content = content;
+        }
+
+        public override string ToString()
+        {
+            return $"{Matter} > {Label} : {Return.ToString("d", CultureInfo.CreateSpecificCulture("fr-FR"))} : {Content}";
+        }
     }
 }
