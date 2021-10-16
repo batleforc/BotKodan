924b05924dfebdaa10dc00732c8219f311b726be
Maxime Leriche <maxleriche.60@gmail.com>
2021-10-16 22:41:17 +0200

    [add] DbTools

---
+++ Tool/DB/startPG.sh
@@ -0,0 +1,1 @@
+docker run --name postgresql -e POSTGRESQL_USERNAME=csharp -e POSTGRESQL_PASSWORD=Caput_Draconis -e POSTGRESQL_DATABASE=Flibbertigibbet -p 5432:5432 -v postgresql3:/bitnami/postgresql bitnami/postgresql:latest
